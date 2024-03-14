94b07b1a5fa9b88d640253717e382cf37ee7af51
hieu7654321 <157661264+hieu7654321@users.noreply.github.com>
2024-03-14 08:18:59 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Bài hát 1", "Một bài hát", "01", 10000, 20);
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Music sheet 2", "Top", "02", 30000, "alan", 20, "Pop");
         book.showInfo();
 
     }
