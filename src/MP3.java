9e0003d5cdbf35ec58c32c29cf4eada8d867121f
hieu7654321 <157661264+hieu7654321@users.noreply.github.com>
2024-03-14 08:18:12 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,25 @@
-public class MP3 {
+public class MP3 extends Item{
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public MP3(String name, String description, String id, float , int duration) {
+        super(name, description, id, price);
+        this.duration = duration;
+    }
+
+    private int duration;
+
+    @Override
+    public void showInfo(){
+        System.out.println("Tên của sản phẩm là: " + name);
+        System.out.println("Mô tả: " + description);
+        System.out.println("ID: " + id);
+        System.out.println("Giá: " + price);
+        System.out.println("Thời lượng: " + duration);
+    }
 }
