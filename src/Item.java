75b96ccba7b0c933e1cef669b0b180d92caa1bad
hieu7654321 <157661264+hieu7654321@users.noreply.github.com>
2024-03-14 08:17:53 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,21 @@
-public class Item {
+public abstract class Item {
+
+    public String name;
+    private String description;
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    protected String id;
+    float price;
+    public Item(String name, String description, String id, float price) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.price = price;
+    }
+    public abstract void showInfo();
 }
